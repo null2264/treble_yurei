c76383bec52758c63462effe5cef24f653b6dfaa
Ahmad Ansori Palembani <palembani@gmail.com>
2026-08-05 13:54:41 +0700

    feat: Make key

--- common.sh
+++ common.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-DEBUG="true"
+readonly DEBUG="true"
+
+readonly signing_keys=(
+    bluetooth
+    gmscompat_lib
+    media
+    networkstack
+    nfc
+    platform
+    releasekey
+    sdk_sandbox
+    shared
+)
